1432ba88ec4684e0da6bd606342fff9376a9c322
Kevin Skoglund <kevin@novafabrica.com>
2024-09-19 15:15:33 +0000

    Add example for argument options hash

---
+++ ruby_examples/arg_options_hash.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+def to_sentence(array, options={})git 
+  join_word = options[:join_word] || 'and'
+  case array.length
+	when 0
+	  ""
+	when 1
+	  array.first.to_s
+	when 2
+		array.join(" #{join_word} ")
+	else
+    array[0..-2].join(', ') + ", #{join_word} #{array[-1]}"
+	end
+end
+
+array = ['red', 'green', 'blue']
+
+puts to_sentence(array)
+puts to_sentence(array, {join_word: 'or'})
