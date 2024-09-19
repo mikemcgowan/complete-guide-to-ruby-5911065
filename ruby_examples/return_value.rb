532e2a9fbfe6f7fec0173828ee2aa20edfcba29c
Kevin Skoglund <kevin@novafabrica.com>
2024-09-19 16:15:49 +0000

    Update example for multiple return values

--- ruby_examples/return_value.rb
+++ ruby_examples/return_value.rb
@@ -7,3 +7,21 @@ def subtract(n1,n2)
 end
 
 puts subtract(8,3)
+
+def add_and_subtract(n1,n2)
+  add = n1 + n2
+  sub = n1 - n2
+  [add, sub]
+end
+
+result = add_and_subtract(8,3)
+a = result[0]
+s = result[1]
+
+puts a
+puts s
+
+a,s = add_and_subtract(8,3)
+
+puts a
+puts s
