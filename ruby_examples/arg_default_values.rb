23343bc14fc5b9a246ba1e87d0f88873fd4345b1
Kevin Skoglund <kevin@novafabrica.com>
2024-09-19 14:54:50 +0000

    Add example for argument default values

---
+++ ruby_examples/arg_default_values.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+def welcome(greeting="Hello", name='world')
+  greeting + ' ' + name + '!'
+end
+git
+puts welcome('Hello', 'neighbor')
+
+puts welcome('Hello')
+
+puts welcome
