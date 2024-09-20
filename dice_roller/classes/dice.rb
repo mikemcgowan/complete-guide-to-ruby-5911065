1e538c2673ec8efd21043b45c899037815ad4412
Kevin Skoglund <kevin@novafabrica.com>
2024-09-20 21:19:33 +0000

    Add solution to Dice

---
+++ dice_roller/classes/dice.rb
@@ -0,0 +1,23 @@
+class Dice
+
+  attr_reader :value
+  
+  def initialize
+    roll
+  end
+  
+  def roll
+    @value = get_a_random_number
+  end
+
+  def display
+    "[ #{value} ]"
+  end
+
+private
+
+  def get_a_random_number
+    rand(6) + 1
+  end
+
+end
---
+++ dice_roller/classes/dice_set.rb
@@ -0,0 +1,15 @@
+class DiceSet
+  
+  def initialize
+    @dice_array = [Dice.new, Dice.new]
+  end
+  
+  def roll
+    @dice_array.each {|dice| dice.roll }
+  end
+
+  def display
+    @dice_array.map {|d| d.display }.join(' - ')
+  end
+  
+end
