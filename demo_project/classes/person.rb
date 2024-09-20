8e666d0fb49ac20ade30973289a881d0b712ff2f
Kevin Skoglund <kevin@novafabrica.com>
2024-09-20 20:11:52 +0000

    Add example of method access control

--- demo_project/classes/person.rb
+++ demo_project/classes/person.rb
@@ -1,7 +1,23 @@
 class Person
 
+  attr_accessor :first_name, :last_name
+
   def say_hello
     puts "Hello!"
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def initial_and_last_name
+    "#{get_initial(first_name)} #{last_name}"
+  end
+
+private
+
+  def get_initial(name)
+    name[0] + "."
   end
 
 end
--- demo_project/init.rb
+++ demo_project/init.rb
@@ -3,8 +3,14 @@
 require_relative 'classes/person'
 require_relative 'classes/animal'
 
-person1 = Person.new
-person2 = Person.new
+person = Person.new
+person.first_name = "Bob"
+person.last_name = "Smith"
+puts person.full_name
+puts person.initial_and_last_name
+# puts person.get_initial("Lucy")
+
+puts "-----"
 
 pig = Animal.new
 
