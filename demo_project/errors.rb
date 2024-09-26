5cf4cc26e8b6fc515e3d81dfd57946fb5d8f92f1
Kevin Skoglund <kevin@novafabrica.com>
2024-09-26 14:44:25 +0000

    Add example of rescue with specific exceptions

--- demo_project/errors.rb
+++ demo_project/errors.rb
@@ -2,12 +2,18 @@
 
 def divide(x,y)
   x/y
-# rescue
-#   "Cannot divide by zero"
+rescue ZeroDivisionError
+  "Cannot divide by zero"
+rescue TypeError
+  "Requires two integers as arguments"
 end
 
 begin
   puts divide(1,0)
+  puts divide(4,"2")
+  puts divide(4)
+rescue ArgumentError
+  puts "Requires two arguments"
 rescue
-  puts "Cannot divide by zero"
+  puts "Something went wrong"
 end
