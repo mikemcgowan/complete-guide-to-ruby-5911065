37bb703348780e0723312e08122626d72d6057b4
Kevin Skoglund <kevin@novafabrica.com>
2024-09-29 16:08:18 +0000

    Add example of reading data from a file

--- ruby_examples/file_access.rb
+++ ruby_examples/file_access.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+# will truncate/overwrite the file each time
 file = File.new('ruby_examples/groceries.txt', 'w')
 
 file.puts "Grocery List"
@@ -9,6 +10,16 @@ file << "+ sugar\n"
 
 file.close
 
-File.open('ruby_examples/groceries.txt', 'r') do
+File.open('ruby_examples/groceries.txt', 'r') do |file|
+
+  # puts file.read(4)
+  # puts file.read(4)
+
+  line1 = file.gets.chomp
+  puts line1.upcase
+
+  file.each_line do |line|
+    puts line.chomp.reverse
+  end
 
 end
