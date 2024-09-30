f3693700bf12bacf0d09582016bee3a97f40198a
Kevin Skoglund <kevin@novafabrica.com>
2024-09-30 14:36:53 +0000

    Add challenge files for Treasure Hunt

---
+++ treasure_hunt/init.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+# Challenge: Treasure Hunt
+#
+# Searches the hunt_dir for files containing "treasure"
+#
+# Outputs the file path when "treasure" is found
+# Example:
+# "Found treasure! buried_treasure/swamp/old_crate.txt"
