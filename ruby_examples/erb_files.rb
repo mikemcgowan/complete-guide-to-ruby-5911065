070afeb685c5e2095f3090b4cf3b1905c100bb7c
Kevin Skoglund <kevin@novafabrica.com>
2024-10-01 19:13:24 +0000

    Add example of ERB template file

--- ruby_examples/erb_files.rb
+++ ruby_examples/erb_files.rb
@@ -4,3 +4,18 @@ require 'erb'
 
 this_dir = File.expand_path(File.dirname(__FILE__))
 template_path = File.join(this_dir, 'inquiry_template.txt.erb')
+
+template = File.read(template_path)
+
+class Customer
+  attr_accessor :first_name, :last_name, :email
+end
+
+@customer = Customer.new
+@customer.first_name = "Ruby"
+@customer.last_name = "Binding"
+@customer.email = "noone@nowhere.com"
+
+output = ERB.new(template).result(binding)
+
+puts output
